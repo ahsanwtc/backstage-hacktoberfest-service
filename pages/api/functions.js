3190e7dea178212606ff582de817ed48314ff521
jsan <ahsan.wtc@gmail.com>
2022-10-11 14:15:38 +0200

    adding /nfts route

--- pages/api/functions.js
+++ pages/api/functions.js
@@ -44,3 +44,45 @@ const mapUserData = users => {
     id: users._id.toString()
   };
 };
+
+export const getNFTs = async nftId => {
+  const client = new MongoClient(getURI());
+  let query = { }, options = { }, data = undefined;
+
+  try {
+    await client.connect();
+    const db = client.db(process.env.DB_NAME);
+    const collection = db.collection('nfts');
+    
+    if (nftId) {
+      // data = await collection.findOne({ _id: ObjectId(userId) });
+    } else {
+      data = await collection.find(query, options).toArray();
+    }
+    
+  } catch (error) {
+    console.log(error);
+  } finally {
+    client.close();
+  }  
+
+  if (!data) {
+    return null;
+  }
+  
+  return mapNFTData(data);
+};
+
+const mapNFTData = nfts => {
+  /* if nft data is an array */
+  if (nfts.length > 0) {
+    return nfts.map(nft => ({
+      id: nft._id.toString(), description: nft.description, image: nft.image, name: nft.name, price: nft.price
+    }));
+  }
+
+  // return {
+  //   first_name: users.first_name, last_name: users.last_name, username: users.username, type: users.type, nfts_owned: users.nfts_owned,
+  //   id: users._id.toString()
+  // };
+};
