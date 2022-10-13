5760c85ff937b37570d76b76e462489ddb331ffc
jsan <ahsan.wtc@gmail.com>
2022-10-13 14:41:57 +0200

    adding some docs

--- pages/docs.js
+++ pages/docs.js
@@ -1,33 +1,41 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import Link from 'next/link';
 import styles from '../styles/Home.module.css';
 
-export default function Home() {
+export default function Docs() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Backstage API Service</title>
-        <meta name="description" content="Backstage API service for database access" />
+        <title>Backstage API | Docs</title>
+        <meta name="description" content="Backstage API service documentation" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://github.com/ahsanwtc/backstage-hacktoberfest-service">Backstage API Service</a>
+          Documentation
         </h1>
 
-        <p className={styles.description}>
-          Endpoints:{' '}
-          <code className={styles.code}>/api/users & /api/nfts</code>
+        <h2>List Users</h2>
+        <p>
+          This method will return an array of users.
         </p>
+        <code className={styles.code}>
+          GET https://backstage-hacktoberfest-service.vercel.app/api/users
+        </code>
+        <h2>Responses 200</h2>
+        
 
-        <div className={styles.grid}>
-          <Link href="/docs" className={styles.card}>
-            <h2 style={{ cursor: 'pointer' }}>Documentation &rarr;</h2>
-          </Link>
+        <h2>List NFTs</h2>
+        <p>
+          This method will return an array of NFTs.
+        </p>
+        <code className={styles.code}>
+          GET https://backstage-hacktoberfest-service.vercel.app/api/nfts
+        </code>
+        <h2>Responses 200</h2>
+        
 
-        </div>
       </main>
 
       <footer className={styles.footer}>
