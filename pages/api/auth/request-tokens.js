ff42ff443e9dc5003ba579130e952cf66e6eddac
jsan <ahsan.wtc@gmail.com>
2022-10-21 14:42:45 +0200

    adding headers for cors

--- pages/api/auth/request-tokens.js
+++ pages/api/auth/request-tokens.js
@@ -22,13 +22,15 @@ export default async function handler(req, res) {
     const result = await fetch(process.env.AUTH0_OAUTH_TOKEN_URL, {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/json'
       },
       body: JSON.stringify(body),
     });
     
     const response = await result.json();
     const { access_token, error } = response;
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     if (error) {
       return res.status(500).json({ error, message: response.error_description });
